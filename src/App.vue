850ae861988a9afb49496c50044dfc615f3fa707
Ftaghn42 <eliott.schwander@gmail.com>
2024-07-25 21:58:26 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -117,12 +117,12 @@ export default {
           "mech": "Fatboy"
         },
         {
-          "callsign": "Starboard",
-          "alias": "Maria",
+          "callsign": "Savage",
+          "alias": "Mikasa",
           "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Scarlet Judicator"
+          "mech": "Into Dust Descend"
         },
         {
           "callsign": "Badlands",
