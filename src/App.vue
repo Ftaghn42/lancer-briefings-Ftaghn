3e5eed0aaa67e52725c2804436228c3bb6557265
Ftaghn42 <eliott.schwander@gmail.com>
2024-08-19 18:22:59 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -89,12 +89,12 @@ export default {
 
   data() {
     return {
-      "mission_slug": "001",
+      "mission_slug": "001b",
       "current_md": "",
       "events": "",
       "missions": [
         {
-          "slug": "001",
+          "slug": "001b",
           "name": "Le Largage",
           "status": "start"
         },
